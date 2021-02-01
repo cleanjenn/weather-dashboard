0213ef143dd2fa2147fbd701dd67b59fff1a4772
jennifer gomez <jennifer23gomez7@gmail.com>
2021-01-31 18:31:33 -0800

    inital pages

---
+++ assets/js /script.js
@@ -0,0 +1,6 @@
+// create variables for each day using moment
+// create function to display weather based on input of city
+// add event listeners to buttons 
+// render city to local storage
+// empty inputs 
+// append..
